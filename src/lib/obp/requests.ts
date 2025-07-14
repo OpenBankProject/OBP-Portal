c63e56ba9025f6a861ee92e7cfbda59e1c3748ef
Nemo Godebski-Pedersen <nemozak1@gmail.com>
2025-07-14 15:08:28 +0530

    fix obp-requests init with docker

--- src/lib/obp/requests.ts
+++ src/lib/obp/requests.ts
@@ -2,179 +2,198 @@ import { env } from '$env/dynamic/public';
 import { OBPErrorBase, OBPRequestError } from '$lib/obp/errors';
 
 class OBPRequests {
-    base_url: string;
+	base_url: string;
 
-    constructor(base_url: string) {
-        console.log(`${this.constructor.name}: `);
-        console.log("Initializing OBPRequests with base URL:", base_url);
+	constructor(base_url: string) {
+		console.log(`${this.constructor.name}: `);
+		console.log('Initializing OBPRequests with base URL:', base_url);
 
-        if (!base_url) {
-            throw new OBPErrorBase("Base URL for OBP requests is not defined.");
-        }
-        this.base_url = base_url
+		if (!base_url) {
+			throw new OBPErrorBase('Base URL for OBP requests is not defined.');
+		}
+		this.base_url = base_url;
 
-        console.log("OBPRequests initialized.");
-        
-    }
+		console.log('OBPRequests initialized.');
+	}
 
-    async get(endpoint: string, accessToken?: string): Promise<any> {
-        console.log(`--------------------------------\n${this.constructor.name}: `);
-        console.debug("GET ", endpoint);
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json'
-        };
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-        const response = await fetch(`${this.base_url}${endpoint}`, {
-            headers
-        });
+	async get(endpoint: string, accessToken?: string): Promise<any> {
+		console.log(`--------------------------------\n${this.constructor.name}: `);
+		console.debug('GET ', endpoint);
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (accessToken) {
+			headers['Authorization'] = `Bearer ${accessToken}`;
+		}
+		const response = await fetch(`${this.base_url}${endpoint}`, {
+			headers
+		});
 
-        const data = await response.json();
-        
-        if (!response.ok) {
-            console.error("Failed to fetch OBP data:", { statusText: response.statusText, data });
-            
-            if (data && data.code && data.message) {
-                throw new OBPRequestError(data.code, data.message);
-            } else {
-                throw new OBPErrorBase(`Error fetching OBP data: ${response.statusText}`);
-            }
-            
-        }
+		const data = await response.json();
 
-        console.debug(`Response from OBP:\n`, response.status, response.statusText);
-        console.log(`--------------------------------`);
-        return data;
-    }
+		if (!response.ok) {
+			console.error('Failed to fetch OBP data:', { statusText: response.statusText, data });
 
-    async post(endpoint: string, body: any, accessToken?: string): Promise<any> {
-        console.log(`${this.constructor.name}: `);
-        console.debug("POST ", endpoint, body);
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json'
-        };
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-        const response = await fetch(`${this.base_url}${endpoint}`, {
-            method: 'POST',
-            headers,
-            body: JSON.stringify(body)
-        });
+			if (data && data.code && data.message) {
+				throw new OBPRequestError(data.code, data.message);
+			} else {
+				throw new OBPErrorBase(`Error fetching OBP data: ${response.statusText}`);
+			}
+		}
 
-        const data = await response.json();
+		console.debug(`Response from OBP:\n`, response.status, response.statusText);
+		console.log(`--------------------------------`);
+		return data;
+	}
 
-        if (!response.ok) {
-            console.error("Failed to post OBP data:", { statusText: response.statusText, data });
-            
-            if (data && data.code && data.message) {
-                throw new OBPRequestError(data.code, data.message);
-            } else {
-                throw new OBPErrorBase(`Error posting OBP data: ${response.statusText}`);
-            }
-        }
+	async post(endpoint: string, body: any, accessToken?: string): Promise<any> {
+		console.log(`${this.constructor.name}: `);
+		console.debug('POST ', endpoint, body);
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (accessToken) {
+			headers['Authorization'] = `Bearer ${accessToken}`;
+		}
+		const response = await fetch(`${this.base_url}${endpoint}`, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(body)
+		});
 
-        console.debug(`Response from OBP:\n`, response.status, response.statusText);
-        console.log(`--------------------------------`);
-        return data;
-    }
+		const data = await response.json();
 
-    async delete(endpoint: string, accessToken?: string): Promise<any> {
-        console.log(`${this.constructor.name}: `);
-        console.debug("DELETE ", endpoint);
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json'
-        };
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-        const response = await fetch(`${this.base_url}${endpoint}`, {
-            method: 'DELETE',
-            headers
-        });
+		if (!response.ok) {
+			console.error('Failed to post OBP data:', { statusText: response.statusText, data });
 
+			if (data && data.code && data.message) {
+				throw new OBPRequestError(data.code, data.message);
+			} else {
+				throw new OBPErrorBase(`Error posting OBP data: ${response.statusText}`);
+			}
+		}
 
-        const data = await response.json();
+		console.debug(`Response from OBP:\n`, response.status, response.statusText);
+		console.log(`--------------------------------`);
+		return data;
+	}
 
-        if (!response.ok) {
-            console.error("Failed to delete OBP data:", response.statusText, data);
-            if (data && data.code && data.message) {
-                throw new OBPRequestError(data.code, data.message);
-            } else {
-                throw new OBPErrorBase(`Error deleting OBP data: ${response.statusText}`);
-            }
-        }
-        
-        console.debug(`Response from OBP:\n`, response.status, response.statusText);
-        console.log(`--------------------------------`);
-        return data;
-    }
+	async delete(endpoint: string, accessToken?: string): Promise<any> {
+		console.log(`${this.constructor.name}: `);
+		console.debug('DELETE ', endpoint);
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (accessToken) {
+			headers['Authorization'] = `Bearer ${accessToken}`;
+		}
+		const response = await fetch(`${this.base_url}${endpoint}`, {
+			method: 'DELETE',
+			headers
+		});
 
-    async put(endpoint: string, body: any, accessToken?: string): Promise<any> {
-        console.log(`${this.constructor.name}: `);
-        console.debug("PUT ", endpoint, body);
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json'
-        };
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-        const response = await fetch(`${this.base_url}${endpoint}`, {
-            method: 'PUT',
-            headers,
-            body: JSON.stringify(body)
-        });
+		const data = await response.json();
 
-        const data = await response.json();
+		if (!response.ok) {
+			console.error('Failed to delete OBP data:', response.statusText, data);
+			if (data && data.code && data.message) {
+				throw new OBPRequestError(data.code, data.message);
+			} else {
+				throw new OBPErrorBase(`Error deleting OBP data: ${response.statusText}`);
+			}
+		}
 
-        if (!response.ok) {
-            console.error("Failed to put OBP data:", { statusText: response.statusText, data });
-            if (data && data.code && data.message) {
-                throw new OBPRequestError(data.code, data.message);
-            } else {
-                throw new OBPErrorBase(`Error putting OBP data: ${response.statusText}`);
-            }
-        }
+		console.debug(`Response from OBP:\n`, response.status, response.statusText);
+		console.log(`--------------------------------`);
+		return data;
+	}
 
-        
-        console.debug(`Response from OBP:\n`, response.status, response.statusText);
-        console.log(`--------------------------------`);
-        return data;
-    }
+	async put(endpoint: string, body: any, accessToken?: string): Promise<any> {
+		console.log(`${this.constructor.name}: `);
+		console.debug('PUT ', endpoint, body);
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (accessToken) {
+			headers['Authorization'] = `Bearer ${accessToken}`;
+		}
+		const response = await fetch(`${this.base_url}${endpoint}`, {
+			method: 'PUT',
+			headers,
+			body: JSON.stringify(body)
+		});
 
-    async patch(endpoint: string, body: any, accessToken?: string): Promise<any> {
-        console.log(`${this.constructor.name}: `);
-        console.debug("PATCH ", endpoint, body);
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json'
-        };
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-        const response = await fetch(`${this.base_url}${endpoint}`, {
-            method: 'PATCH',
-            headers,
-            body: JSON.stringify(body)
-        });
+		const data = await response.json();
 
-        const data = await response.json();
+		if (!response.ok) {
+			console.error('Failed to put OBP data:', { statusText: response.statusText, data });
+			if (data && data.code && data.message) {
+				throw new OBPRequestError(data.code, data.message);
+			} else {
+				throw new OBPErrorBase(`Error putting OBP data: ${response.statusText}`);
+			}
+		}
 
-        if (!response.ok) {
-            console.error("Failed to patch OBP data:", { statusText: response.statusText, data });
-            if (data && data.code && data.message) {
-                throw new OBPRequestError(data.code, data.message);
-            } else {
-                throw new OBPErrorBase(`Error patching OBP data: ${response.statusText}`);
-            }
-        }
+		console.debug(`Response from OBP:\n`, response.status, response.statusText);
+		console.log(`--------------------------------`);
+		return data;
+	}
 
-        console.debug(`Response from OBP:\n`, response.status, response.statusText);
-        console.log(`--------------------------------`);
-        return data;
-    }
+	async patch(endpoint: string, body: any, accessToken?: string): Promise<any> {
+		console.log(`${this.constructor.name}: `);
+		console.debug('PATCH ', endpoint, body);
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (accessToken) {
+			headers['Authorization'] = `Bearer ${accessToken}`;
+		}
+		const response = await fetch(`${this.base_url}${endpoint}`, {
+			method: 'PATCH',
+			headers,
+			body: JSON.stringify(body)
+		});
 
+		const data = await response.json();
 
+		if (!response.ok) {
+			console.error('Failed to patch OBP data:', { statusText: response.statusText, data });
+			if (data && data.code && data.message) {
+				throw new OBPRequestError(data.code, data.message);
+			} else {
+				throw new OBPErrorBase(`Error patching OBP data: ${response.statusText}`);
+			}
+		}
+
+		console.debug(`Response from OBP:\n`, response.status, response.statusText);
+		console.log(`--------------------------------`);
+		return data;
+	}
 }
 
-export const obp_requests = new OBPRequests(env.PUBLIC_OBP_BASE_URL);
+let obp_requests_instance: OBPRequests | null = null;
+
+export const obp_requests = {
+	get instance(): OBPRequests {
+		if (!obp_requests_instance) {
+			obp_requests_instance = new OBPRequests(env.PUBLIC_OBP_BASE_URL);
+		}
+		return obp_requests_instance;
+	},
+
+	get: function (endpoint: string, accessToken?: string) {
+		return this.instance.get(endpoint, accessToken);
+	},
+
+	post: function (endpoint: string, data: any, accessToken?: string) {
+		return this.instance.post(endpoint, data, accessToken);
+	},
+
+	put: function (endpoint: string, data: any, accessToken?: string) {
+		return this.instance.put(endpoint, data, accessToken);
+	},
+
+	delete: function (endpoint: string, accessToken?: string) {
+		return this.instance.delete(endpoint, accessToken);
+	}
+};
