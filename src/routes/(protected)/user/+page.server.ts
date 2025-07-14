81b49ce9f80c7466a188913d5b75e2306998a51e
Nemo Godebski-Pedersen <nemozak1@gmail.com>
2025-07-14 18:20:11 +0530

    add custom error page

--- src/routes/(protected)/user/+page.server.ts
+++ src/routes/(protected)/user/+page.server.ts
@@ -1,6 +1,7 @@
 import type { RequestEvent, Actions } from "@sveltejs/kit";
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 
 export function load(event: RequestEvent) {
-    throw redirect(307, "/user/consents")
+    // For now throw a 404 as this page is not ready yet
+    throw error(503, "Woops! This page is under construction.");
 }
