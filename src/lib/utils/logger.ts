845d809690c43a51bb620d4c6e6aabf64ea0bedd
simonredfern <simon@tesobe.com>
2025-08-13 14:42:50 +0200

    logger related

---
+++ src/lib/utils/logger.ts
@@ -0,0 +1,52 @@
+export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
+
+export interface Logger {
+  debug(...args: any[]): void;
+  info(...args: any[]): void;
+  warn(...args: any[]): void;
+  error(...args: any[]): void;
+}
+
+class ConsoleLogger implements Logger {
+  constructor(private context: string) {}
+
+  private log(level: LogLevel, ...args: any[]): void {
+    const timestamp = new Date().toISOString();
+    const prefix = `[${timestamp}] [${level.toUpperCase()}] [${this.context}]`;
+    
+    switch (level) {
+      case 'debug':
+        console.debug(prefix, ...args);
+        break;
+      case 'info':
+        console.log(prefix, ...args);
+        break;
+      case 'warn':
+        console.warn(prefix, ...args);
+        break;
+      case 'error':
+        console.error(prefix, ...args);
+        break;
+    }
+  }
+
+  debug(...args: any[]): void {
+    this.log('debug', ...args);
+  }
+
+  info(...args: any[]): void {
+    this.log('info', ...args);
+  }
+
+  warn(...args: any[]): void {
+    this.log('warn', ...args);
+  }
+
+  error(...args: any[]): void {
+    this.log('error', ...args);
+  }
+}
+
+export function createLogger(context: string): Logger {
+  return new ConsoleLogger(context);
+}
