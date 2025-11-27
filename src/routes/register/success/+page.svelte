e313b1be2715e4a3687a623d822979cea4174af2
simonredfern <simon@tesobe.com>
2025-11-27 17:36:16 +0100

    add message about checking email

--- src/routes/register/success/+page.svelte
+++ src/routes/register/success/+page.svelte
@@ -8,7 +8,10 @@
 	class="card preset-filled-primary-100-900 border-primary-200-800 divide-primary-200-800 mx-auto my-10 flex max-w-md flex-col divide-y border-[1px] shadow-lg sm:max-w-2xl lg:max-w-3xl"
 >
 	<header class="py-4">
-		<h1 class="h4 text-center">User registration success</h1>
+		<h1 class="h4 text-center">User Registration Success</h1>
+		<p class="text-center mt-2 text-sm text-surface-700-300">
+			Please check your email to validate your email address.
+		</p>
 	</header>
 	<article class="space-y-4 p-4">
         <div class="preset-filled-primary-50-950 shadow-md rounded-lg p-4 m-1.5">
